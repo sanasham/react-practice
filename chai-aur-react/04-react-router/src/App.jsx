04cb8e9c9e34b880bc033ba276dee8e679c8f75d
sanasham <sayyedesubvali@gmail.com>
2024-01-29 22:59:44 +0530

    router project added

---
+++ chai-aur-react/04-react-router/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>test</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
