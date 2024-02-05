afe1fc7512f4762ac17bcadab76a8d576aa1351e
sanasham <sayyedesubvali@gmail.com>
2024-02-05 22:54:01 +0530

    mega project added

---
+++ chai-aur-react/12-Megablog/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL);
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
