e6b77f86787908e44394b1e0a565cbdbba5480b3
sanasham <sayyedesubvali@gmail.com>
2024-11-19 18:27:15 +0530

    sorting functionality added

--- seema-holiday-developer/sorting/src/components/Sorting.jsx
+++ seema-holiday-developer/sorting/src/components/Sorting.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./Sorting.css";
 
 function Sorting() {
-  return <div>Sorting Component</div>;
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        if (response.status === 200) {
+          setData(response.data);
+          console.log(data);
+        }
+      })
+
+      .catch((err) => console.log(`unexpected error occuered ${err}`));
+  }, []);
+
+  const sortData = (field) => {
+    const sortedData = [...data].sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a[field] > b[field] ? 1 : -1;
+      } else {
+        return a[field] < b[field] ? 1 : -1;
+      }
+    });
+    setData(sortedData);
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+  return (
+    <>
+      <h1>Sorting Component</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Id
+              <button onClick={() => sortData("id")}>
+                Sort {sortOrder === "asc" ? "↑" : "↓"}
+              </button>
+            </th>
+            <th>title</th>
+            <th>userId</th>
+            <th>completed</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((item, index) => {
+            return (
+              <tr key={item.id + Math.random(0)}>
+                <td key={item.id + item.userId + index + Math.random(10)}>
+                  {item.id}
+                </td>
+                <td key={item.id + item.userId + index + Math.random(10)}>
+                  {item.title}
+                </td>
+                <td key={item.id + item.userId + index + Math.random(10)}>
+                  {item.userId}
+                </td>
+                <td key={item.id + item.userId + index + Math.random(10)}>
+                  {item.completed.toString()}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Sorting;
