5a797c320ed4e5a7e49390f2cb51ab7d2420b9cb
sanasham <sayyedesubvali@gmail.com>
2024-11-20 07:13:32 +0530

    sorting functionality added

--- seema-holiday-developer/sorting/src/components/Sorting.jsx
+++ seema-holiday-developer/sorting/src/components/Sorting.jsx
@@ -5,13 +5,15 @@ import "./Sorting.css";
 function Sorting() {
   const [sortOrder, setSortOrder] = useState("asc");
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
       .then((response) => {
         if (response.status === 200) {
           setData(response.data);
-          console.log(data);
+          setFilteredData(response.data);
         }
       })
 
@@ -26,12 +28,30 @@ function Sorting() {
         return a[field] < b[field] ? 1 : -1;
       }
     });
-    setData(sortedData);
+    setFilteredData(sortedData);
     setSortOrder(sortOrder === "asc" ? "desc" : "asc");
   };
+
+  const handleSearch = (e) => {
+    const searchValue = e.target.value.toLowerCase();
+    console.log(searchValue);
+
+    // setData(data.filter(item => item.title.toLowerCase().includes(searchValue)));
+
+    console.log("handle search");
+    setSearchTerm(searchValue);
+
+    const filtered = data.filter((item) =>
+      item.title.toLowerCase().includes(searchValue)
+    );
+    console.log("filtered", filtered);
+    setFilteredData(filtered);
+  };
+
   return (
     <>
       <h1>Sorting Component</h1>
+      <input type="text" onChange={handleSearch} value={searchTerm} />
       <table>
         <thead>
           <tr>
@@ -41,31 +61,37 @@ function Sorting() {
                 Sort {sortOrder === "asc" ? "↑" : "↓"}
               </button>
             </th>
-            <th>title</th>
+            <th>
+              title
+              <button onClick={() => sortData("title")}>
+                Sort {sortOrder === "asc" ? "↑" : "↓"}
+              </button>
+            </th>
             <th>userId</th>
             <th>completed</th>
           </tr>
         </thead>
 
         <tbody>
-          {data.map((item, index) => {
-            return (
-              <tr key={item.id + Math.random(0)}>
-                <td key={item.id + item.userId + index + Math.random(10)}>
-                  {item.id}
-                </td>
-                <td key={item.id + item.userId + index + Math.random(10)}>
-                  {item.title}
-                </td>
-                <td key={item.id + item.userId + index + Math.random(10)}>
-                  {item.userId}
-                </td>
-                <td key={item.id + item.userId + index + Math.random(10)}>
-                  {item.completed.toString()}
-                </td>
-              </tr>
-            );
-          })}
+          {filteredData &&
+            filteredData.map((item, index) => {
+              return (
+                <tr key={item.id + Math.random(0)}>
+                  <td key={item.id + item.userId + index + Math.random(10)}>
+                    {item.id}
+                  </td>
+                  <td key={item.id + item.userId + index + Math.random(10)}>
+                    {item.title}
+                  </td>
+                  <td key={item.id + item.userId + index + Math.random(10)}>
+                    {item.userId}
+                  </td>
+                  <td key={item.id + item.userId + index + Math.random(10)}>
+                    {item.completed.toString()}
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </>
